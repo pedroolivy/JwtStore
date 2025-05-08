c370f85b9e161f5112237c555ddf38e1bbea68d7
Pedro <pedrooliveiradevs@gmail.com>
2025-05-07 22:40:03 -0300

    Criando mapeamento com UF para as entidades e os valueObjects

---
+++ JwtStore.Infra/AccountContext/Mappings/UserMap.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using JwtStore.Core.AccountContext.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JwtStore.Infra.AccountContext.Mappings
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("users");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .HasColumnName("name")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(120)
+                .IsRequired(true);
+
+            builder.Property(x => x.Image)
+                .HasColumnName("Image")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(255)
+                .IsRequired(true);
+
+            builder.OwnsOne(x => x.Email)
+                .Property(x => x.Address)
+                .HasColumnName("Email")
+                .IsRequired(true);
+
+            builder.OwnsOne(x => x.Email)
+                .OwnsOne(x => x.Verification)
+                .Property(x => x.Code)
+                .HasColumnName("EmailVerificationCode")
+                .IsRequired(true);
+
+            builder.OwnsOne(x => x.Email)
+                .OwnsOne(x => x.Verification)
+                .Property(x => x.ExpiresAt)
+                .HasColumnName("EmailVerificationExpiresAt")
+                .IsRequired(false);
+
+            builder.OwnsOne(x => x.Email)
+                .OwnsOne(x => x.Verification)
+                .Property(x => x.VerifiedAt)
+                .HasColumnName("EmailVerificationVerifiedAt")
+                .IsRequired(false);
+
+            builder.OwnsOne(x => x.Email)
+                .OwnsOne(x => x.Verification)
+                .Ignore(x => x.IsActive);
+
+            builder.OwnsOne(x => x.Password)
+                .Property(x => x.Hash)
+                .HasColumnName("PasswordHash")
+                .IsRequired();
+
+            builder.OwnsOne(x => x.Password)
+                .Property(x => x.ResetCode)
+                .HasColumnName("PasswordResetCode")
+                .IsRequired();
+        }
+    }
+}
